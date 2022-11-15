a3ffa7f7e4d4848ac40300b2c45111f15b33b948
Rezurgat <Killa.S.V.ru@bk.ru>
2022-11-15 20:30:03 +0300

    Refactored admin for Task model; Added listview; refactored urls(whole project);started work with template (task_list.html)

--- MeDO/todo/views.py
+++ MeDO/todo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
