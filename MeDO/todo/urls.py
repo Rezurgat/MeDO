ef5916fbe56dc6de23007605d8021388429f8fd2
Rezurgat <Killa.S.V.ru@bk.ru>
2022-11-28 20:43:38 +0300

    Working with task-create

--- MeDO/todo/urls.py
+++ MeDO/todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='task_list'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+
 
 
 ]
